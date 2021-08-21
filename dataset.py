b3b2cf922cc0be623b137d889657f360453a2cdf
Palandr1234 <a.palaev@innopolis.university>
2021-08-22 00:22:55 +0300

    Dataset class added

--- dataset.py
+++ dataset.py
@@ -1,7 +1,15 @@
 import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from PIL import Image
+import pandas as pd
+import os
 import spacy
 
 spacy_eng = spacy.load("en")
+start_token = "<SOS>"
+end_token = "<EOS>"
+unk_token = "<UNK>"
 
 
 class Vocabulary:
@@ -25,7 +33,6 @@ class Vocabulary:
     @staticmethod
     def tokenize(text: str) -> list[str]:
         """
-        tokenize the text using spacy tokenizer
         :param text: input text to be tokenized
         :return: tokenized text
         """
@@ -56,7 +63,6 @@ class Vocabulary:
 
     def numericalize(self, text: str) -> list[int]:
         """
-        numericalize the text
         :param text: text to be numericalized
         :return: numericalized text
         """
@@ -65,6 +71,54 @@ class Vocabulary:
 
         # convert all tokens to indices
         return [
-            self.wrd2idx[token] if token in self.wrd2idx else self.wrd2idx["<UNk>"]
+            self.wrd2idx[token] if token in self.wrd2idx else self.wrd2idx[unk_token]
             for token in tokenized_text
         ]
+
+
+class FlickrDataset(Dataset):
+    def __init__(self, root: str, captions_file: str, transform: transforms.Compose = None, occur_threshold: int = 5)\
+            -> None:
+        """
+        Initialize the dataset
+        :param root: root directory
+        :param captions_file: file with image filenames and their appropriate captions
+        :param transform: transform to be applies to the image
+        :param occur_threshold: threshold for a word to be added to the vocabulary
+        """
+        self.root = root
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+
+        self.img_filenames = self.df["image"]
+        self.captions = self.df["caption"]
+
+        self.vocab = Vocabulary(occur_threshold)
+        # build vocabulary from the captions
+        self.vocab.build_vocabulary(self.captions.tolist())
+
+    def __len__(self) -> int:
+        """
+        :return: length of the dataset
+        """
+        return len(self.df)
+
+    def __getitem__(self, index: int) -> (Image, torch.tensor):
+        """
+        :param index: index of the item
+        :return: the image and the caption corresponding to that index
+        """
+        caption = self.captions[index]
+        img_id = self.img_filenames[index]
+        img = Image.open(os.path.join(self.root, img_id)).convert("RGB")
+
+        # apply transforms to the image if there are any
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # add the start token and the end token to the numericalized text
+        numericalized_caption = [self.vocab.wrd2idx[start_token]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.wrd2idx[end_token])
+
+        return img, torch.tensor(numericalized_caption)
