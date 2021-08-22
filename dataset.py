a080675f2a7487674d13cab2374adb89c13b0af5
Palandr1234 <a.palaev@innopolis.university>
2021-08-22 12:47:45 +0300

    Comments added

--- dataset.py
+++ dataset.py
@@ -147,7 +147,19 @@ class MyCollate:
         return imgs, targets
 
 
-def get_dataloader(root, annotation_file, transform=None, batch_size=32, num_workers=8, shuffle=True, pin_memory=True):
+def get_dataloader(root: str, annotation_file: str, transform: transforms.Compose = None, batch_size: int = 32,
+                   num_workers: int = 8, shuffle: bool = True, pin_memory: bool = True) \
+        -> tuple[FlickrDataset, DataLoader]:
+    """
+    :param root: directory with the images
+    :param annotation_file: file with captions for each image file
+    :param transform: transform to be applied to the image
+    :param batch_size: size of the batch
+    :param num_workers: number of loader worker processes
+    :param shuffle:whether to shuffle the dataset or not
+    :param pin_memory: whether to automatically put the fetched data Tensors in pinned memory or not
+    :return: Dataloader with the images and captions
+    """
     dataset = FlickrDataset(root, annotation_file, transform=transform)
 
     pad_idx = dataset.voc.wrd2idx[pad_token]
