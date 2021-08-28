1b0e95034656744fc138b7bd1760249d05259725
Palandr1234 <palandr545@gmail.com>
2021-08-28 21:33:40 +0300

    Checkpoint saving added

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 
@@ -14,3 +15,13 @@ def load_checkpoint(checkpoint: dict, model: nn.Module, optimizer: optim.Optimiz
     optimizer.load_state_dict(checkpoint["optimizer"])
     step = checkpoint["step"]
     return step
+
+
+def save_checkpoint(checkpoint: dict, checkpoint_path: str = "checkpoint.pth") -> None:
+    """
+    Saves checkpoints
+    :param checkpoint: Dictionary with model state dict, optimizer state dict and step number
+    :param checkpoint_path: Path where checkpoint should be saved
+    :return: None
+    """
+    torch.save(checkpoint, checkpoint_path)
